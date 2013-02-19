7d5dc69c82a0474466f6ca94e56fe91594e7673a
Giovani Moutinho <giovanim@ymail.com>
2013-02-19 11:32:23 -0800

    Create Menu.py

---
+++ Menu.py
@@ -0,0 +1,24 @@
+#encoding: utf-8
+
+
+print "\t        ─▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄─ "
+print "\t         █░░░█░░░░░░░░░░▄▄░██░█ "
+print "\t         █░▀▀█▀▀░▄▀░▄▀░░▀▀░▄▄░█ "
+print "\t         █░░░▀░░░▄▄▄▄▄░░██░▀▀░█"
+print "\t        ─▀▄▄▄▄▄▀─────▀▄▄▄▄▄▄▀─"
+
+opcao=input ("\n\n\t\tSelecione o jogo desejado: \n\t\t1-Jogo da Velha \n\t\t2-Jokenpô \n\t\t3-Par ou Impar \n\t\t4-Quiz \n\t\t5-Sobre \n\t\t6-Sair\n\n")
+
+os.system(['clear','cls'][os.name == 'nt'])
+if opcao==1:
+	print "Coloque seu codigo aqui"
+elif opcao==2:
+	print "Coloque seu codigo aqui"
+elif opcao==3:
+	print "Coloque seu codigo aqui"
+elif opcao==4:
+	print "Coloque seu codigo aqui"
+elif opcao==5:
+	print "Coloque seu codigo aqui"
+else:
+	print "Encerrando..."
